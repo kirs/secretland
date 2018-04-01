c3d9cd5d221a49b9907823f865ee73833be58dca
Kir Shatrov <shatrov@me.com>
2018-04-01 13:19:43 +0100

    Add slowpath

--- app/controllers/helloworld_controller.rb
+++ app/controllers/helloworld_controller.rb
@@ -2,4 +2,15 @@ class HelloworldController < ApplicationController
   def index
     @credentials = Rails.application.credentials.config
   end
+
+  def slow
+    render plain: fibonacci(37)
+  end
+
+  private
+
+  def fibonacci(n)
+    return  n  if n <= 1
+    fibonacci( n - 1 ) + fibonacci( n - 2 )
+  end
 end
